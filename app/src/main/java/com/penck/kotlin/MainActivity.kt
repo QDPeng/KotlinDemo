e1f773c0dd7f72c4c4fda857556e28eebba0329d
lurkingliu <liusp_008@sina.com>
2017-06-15 11:02:39 +0800

    add picture library

--- app/src/main/java/com/penck/kotlin/MainActivity.kt
+++ app/src/main/java/com/penck/kotlin/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.penck.kotlin
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Toast
 import com.penck.kotlin.notification.NotifyActivity
+import com.penck.picinpic.VideoActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,7 +24,9 @@ class MainActivity : AppCompatActivity() {
     fun onViewClick(view: View) {
         when (view.id) {
             R.id.notification_channel -> startActivity(NotifyActivity.getStartActivityIntent(this))
-            R.id.autofill_framework -> Unit
+            R.id.picture_in_picture -> {
+                startActivity(Intent(this@MainActivity, VideoActivity::class.java))
+            }
             else -> Toast.makeText(this, "do nothing", Toast.LENGTH_LONG).show()
         }
     }
